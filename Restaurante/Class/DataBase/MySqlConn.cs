f6b6075ebe6fe1f06eba44c48aeef4ee38d190a2
Felipe <menix1406@gmail.com>
2019-04-27 17:52:23 -0300

    Firt commit, initing with class of connect to database

---
+++ Restaurante/Class/DataBase/MySqlConn.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Importing MySQL Dll
+using MySql.Data.MySqlClient;
+
+
+namespace Restaurante.Class.DataBase {
+    class MySqlConn {
+
+        #region Properties
+
+        string stringConn;
+
+        MySqlConnection connDB;
+
+        #endregion
+
+
+        #region Construct
+
+        //Construct for connection in MySQL and in DataBase
+        public MySqlConn(string server, string vUser, string vPassword, string vDataBase) {
+
+            string stringConn = "server=" + server + ";database=" + vDataBase + ";User Id=" + vUser + ";Password=" + vPassword + ";";
+
+            connDB = new MySqlConnection(stringConn);
+
+            connectDB();
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        /*--------------------
+        Method 1 (SQLQuery)
+        Param1 = Query
+        Method for capture query 
+        --------------------*/
+        public void SQLQuery(string SQLq) {
+
+            try {
+                MySqlCommand myCommand = new MySqlCommand(SQLq, connDB);
+            }
+            catch (Exception) {
+                Console.WriteLine("Sem Sucesso !");
+                throw;
+            }
+        }
+
+
+        /*--------------------
+        Method 1 (SQLQuery)
+        Method for connect to database
+        --------------------*/
+        public MySqlConnection connectDB() {
+
+            switch (connDB.State == System.Data.ConnectionState.Closed) {
+                case true:
+                    connDB.Open();
+                    break;
+                default:
+                    break;
+            }
+            return connDB;
+        }
+
+
+        /*--------------------
+       Method 1 (SQLQuery)
+       Method for disconnect to database
+       --------------------*/
+        public void disconnectDB() {
+
+            switch (connDB.State == System.Data.ConnectionState.Open) {
+                case true:
+                    connDB.Close();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    #endregion
+
+}
